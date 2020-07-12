efdfeb3c1980a173dcda1ac0f034e7151d1c4e2c
Brian Gantzler <brian.gantzler@cardinalhealth.com>
2020-07-12 10:43:34 -0400

    removed root pages of demo

--- tests/dummy/app/routes/send-component.js
+++ tests/dummy/app/routes/send-component.js
@@ -1,4 +1,10 @@
+import EmberObject from '@ember/object';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return EmberObject.create({
+      title: "Welcome"
+    });
+  }
 });
